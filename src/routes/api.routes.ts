d18795ded8fd49015b7d9b55425cc501ce0d290c
avtandil <shainidze@gmail.com>
2025-07-10 08:13:09 +0300

    feat: add sever

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -1,7 +1,26 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+
+import * as googleTTS from 'google-tts-api';
+import fs from 'fs';
+import https from 'https';
 
 const router = Router();
 
-// router.get('/analyze', () => {}); // api/analyze
+router.get('/tts', async (req: Request, res: Response) => {
+  const text = req.query.text as string;
+
+  try {
+    const base64 = await googleTTS.getAudioBase64(text, {
+      lang: 'ru',
+      slow: false,
+      host: 'https://translate.google.com',
+      timeout: 10000,
+    });
+    res.json({ base64 });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}); // api/tts
 
 export { router };
